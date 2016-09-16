d00035d6b16d9dd8f044fb705cb90fe263b87c78
Alexandre Ignjatovic <aignjatovic@octo.com>
2016-09-16 17:13:27 +0200

    Lazily build importers and storage

--- lib/video/worker.rb
+++ lib/video/worker.rb
@@ -4,36 +4,54 @@ $LOAD_PATH.unshift('lib')
 require 'sidekiq'
 require 'fileutils'
 require 'temp_folder'
-require 'video/importer'
-require 'video/importer/youtube'
-require 'video/storage'
-require 'video/storage/file'
 
 class Video
   # Worker in charge of draining and saving videos
   class Worker
     include Sidekiq::Worker
+    class << self
+      def storage
+        @storage ||= storage_impl
+      end
+
+      def importers
+        @importers ||= importers_impl
+      end
+
+      private
+
+      def storage_impl
+        require 'video/storage'
+        require 'video/storage/file'
+        Video::Storage::File.new(ROOT).as_a(Video::Storage).freeze
+      end
+
+      def importers_impl
+        require 'video/importer'
+        require 'video/importer/youtube'
+        {
+          youtube: Video::Importer::Youtube.new.as_a(Video::Importer)
+        }.freeze
+      end
+    end
 
     ROOT_KEY = 'VIDEO_STORAGE_LOCAL_ROOT'
     ROOT = ENV[ROOT_KEY] || raise("Missing env var #{ROOT_KEY}")
-    STORAGE = Video::Storage::File.new(ROOT).as_a(Video::Storage)
 
     def perform(url)
       puts "Trying to retrieve url #{url}"
       TempFolder.host_process('./tmp') do
-        STORAGE.process(Video.new(importer_for(url).process(url)))
+        self.class.storage.process(Video.new(importer_for(url).process(url)))
       end
     end
 
     private
 
-    IMPORTERS = {
-      youtube: Video::Importer::Youtube.new.as_a(Video::Importer)
-    }.freeze
-
     def importer_for(url)
       # Quite lame, but that's a start
-      IMPORTERS.each { |token, importer| return importer if url.index(token) }
+      self.class.importers.each do |token, importer|
+        return importer if url.index(token)
+      end
       raise 'Importer not found'
     end
   end
